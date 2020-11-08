8bbfd0007a4d0eaa5bdc86252c122352daad4939
damianfixel <damianfixel@gmail.com>
2020-11-08 12:50:11 -0300

    delete combine reducer

--- src/redux/CombineReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import withReduxState from './Reducer'
-import loader from './loader/Reducer'
-
-export default combineReducers({
-    withReduxState,
-    loader
-})
